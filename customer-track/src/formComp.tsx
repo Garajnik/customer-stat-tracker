8204ac26950e01b2f043d68c192d9949bdfeb10c
garajnik <lexan1699@gmail.com>
2023-12-01 23:57:26 +0800

    Finally working

--- customer-track/src/formComp.tsx
+++ customer-track/src/formComp.tsx
@@ -20,7 +20,9 @@ const FormComp = () => {
     EndTime: string;
   }
 
-  const [arrTime, setArrTime] = useState("");
+  const [, setArrTime] = useState("");
+  const [, setStartTime] = useState("");
+  const [, setEndTime] = useState("");
   const [items, setItems] = useState([new ItemProp(1, "", "", "")]);
 
   function LogAllData() {
@@ -37,16 +39,21 @@ const FormComp = () => {
     return `${currentHour}:${currentMinute}:${currentSecond}`;
   }
 
+  function removeItem(id: number) {
+    const newItems = items.filter((item) => item.id !== id);
+    setItems(newItems);
+  }
+
   return (
     <>
-      {items.map((item) => (
+      {items.map((item, index) => (
         <form key={item.id}>
           <label>
             Время прибытия:
             <input
               type="text"
               placeholder="Время прибытия"
-              value={arrTime}
+              value={item.ArriveTime}
               onChange={(event: React.FormEvent<HTMLInputElement>) => {
                 setArrTime(event.currentTarget.value);
                 item.ArriveTime = event.currentTarget.value;
@@ -55,8 +62,15 @@ const FormComp = () => {
             <button
               type="button"
               onClick={() => {
-                item.ArriveTime = GetTime();
-                setArrTime(item.ArriveTime);
+                const newTime = GetTime();
+                const newItems = [...items];
+                newItems[index] = new ItemProp(
+                  item.id,
+                  newTime,
+                  item.StartTime,
+                  item.EndTime
+                );
+                setItems(newItems);
               }}
             >
               Авто
@@ -67,33 +81,75 @@ const FormComp = () => {
             <input
               type="text"
               placeholder="Начало обслуживания"
+              value={item.StartTime}
               onChange={(event: React.FormEvent<HTMLInputElement>) => {
+                setStartTime(event.currentTarget.value);
                 item.StartTime = event.currentTarget.value;
               }}
             />
-            <button type="button">Авто</button>
+            <button
+              type="button"
+              onClick={() => {
+                const newTime = GetTime();
+                const newItems = [...items];
+                newItems[index] = new ItemProp(
+                  item.id,
+                  item.ArriveTime,
+                  newTime,
+                  item.EndTime
+                );
+                setItems(newItems);
+              }}
+            >
+              Авто
+            </button>
           </label>
           <label>
             Конец обслуживания:
             <input
               type="text"
               placeholder="Конец обслуживания"
+              value={item.EndTime}
               onChange={(event: React.FormEvent<HTMLInputElement>) => {
+                setEndTime(event.currentTarget.value);
                 item.EndTime = event.currentTarget.value;
               }}
             />
-            <button type="button">Авто</button>
+            <button
+              type="button"
+              onClick={() => {
+                const newTime = GetTime();
+                const newItems = [...items];
+                newItems[index] = new ItemProp(
+                  item.id,
+                  item.ArriveTime,
+                  item.StartTime,
+                  newTime
+                );
+                setItems(newItems);
+              }}
+            >
+              Авто
+            </button>
           </label>
+          <button type="button" onClick={() => removeItem(item.id)}>
+            Удалить
+          </button>
         </form>
       ))}
       <button onClick={LogAllData}>Вывести данные</button>
       <button
         onClick={() => {
-          setItems([...items, new ItemProp(items.length + 1, "", "", "")]);
+          let highestId = Math.max(...items.map((item) => item.id));
+          if (highestId == -Infinity) {
+            highestId = 0;
+          }
+          setItems([...items, new ItemProp(highestId + 1, "", "", "")]);
         }}
       >
         Добавить строку
       </button>
+      <span>Осталось добавить: {100 - items.length}</span>
     </>
   );
 };
