7968c5fd254b7e93d883d0ca4f20985a29b33489
garajnik <lexan1699@gmail.com>
2023-12-01 20:13:40 +0800

    Almost done with input

--- customer-track/src/App.tsx
+++ customer-track/src/App.tsx
@@ -1,51 +1,9 @@
-import React, { useState, useEffect } from "react";
-import FormComp from "./formComp";
+import FormComp from "./formComp.tsx";
 
-interface AppProps {}
-
-const App: React.FC<AppProps> = () => {
-  // Используем локальное хранилище для сохранения/восстановления массива компонентов
-  const storedComponents = localStorage.getItem("formComponents");
-  const initialComponents: JSX.Element[] = storedComponents
-    ? JSON.parse(storedComponents)
-    : [];
-
-  // Состояние для хранения массива компонентов
-  const [formComponents, setFormComponents] =
-    useState<JSX.Element[]>(initialComponents);
-
-  // Функция для добавления нового компонента в массив
-  const addFormComponent = () => {
-    setFormComponents((prevComponents) => [
-      ...prevComponents,
-      <FormComp key={prevComponents.length + 1}>
-        <button id="deleteBtn">Удалить строку</button>
-      </FormComp>,
-    ]);
-  };
-
-  // Сохраняем массив компонентов в локальное хранилище при каждом изменении
-  useEffect(() => {
-    localStorage.setItem("formComponents", JSON.stringify(formComponents));
-  }, [formComponents]);
-
+export default function App() {
   return (
-    <div>
-      {/* Вывод компонентов из массива */}
-      {formComponents.map((form) => (
-        <FormComp key={prevComponents.length + 1}>
-          <button id="deleteBtn">Удалить строку</button>
-        </FormComp>
-      ))}
-
-      {/* Кнопка для добавления нового компонента в массив */}
-      <button onClick={addFormComponent}>Добавить строку</button>
-    </div>
+    <>
+      <FormComp></FormComp>
+    </>
   );
-};
-
-export default App;
-
-// <FormComp key={prevComponents.length + 1}>
-// <button id="deleteBtn">Удалить строку</button>
-// </FormComp>
+}
--- customer-track/src/formComp.tsx
+++ customer-track/src/formComp.tsx
@@ -1,37 +1,101 @@
-import { ReactNode } from "react";
-import "./app.css";
+import { useState } from "react";
+import "./App.css";
 
-interface formProps {
-  children: ReactNode;
-}
+const FormComp = () => {
+  class ItemProp {
+    constructor(
+      id: number,
+      ArriveTime: string,
+      StartTime: string,
+      EndTime: string
+    ) {
+      this.id = id;
+      this.ArriveTime = ArriveTime;
+      this.StartTime = StartTime;
+      this.EndTime = EndTime;
+    }
+    id: number;
+    ArriveTime: string;
+    StartTime: string;
+    EndTime: string;
+  }
 
-export default function FormComp({ children }: formProps) {
+  const [arrTime, setArrTime] = useState("");
+  const [items, setItems] = useState([new ItemProp(1, "", "", "")]);
+
+  function LogAllData() {
+    items.forEach((value) => {
+      console.log(value);
+    });
+  }
+
+  function GetTime() {
+    const currentDate = new Date();
+    const currentHour = currentDate.getHours();
+    const currentMinute = currentDate.getMinutes();
+    const currentSecond = currentDate.getSeconds();
+    return `${currentHour}:${currentMinute}:${currentSecond}`;
+  }
+
   return (
-    <div id="content-holder">
-      {children}
-      <div className="input-container">
-        <input placeholder="Время прихода" />
-        <div className="button-container">
-          <button>Авто</button>
-          <button>Удалить</button>
-        </div>
-      </div>
-
-      <div className="input-container">
-        <input placeholder="Начало обслуживания" />
-        <div className="button-container">
-          <button>Авто</button>
-          <button>Удалить</button>
-        </div>
-      </div>
-
-      <div className="input-container">
-        <input placeholder="Конец обслуживания" />
-        <div className="button-container">
-          <button>Авто</button>
-          <button>Удалить</button>
-        </div>
-      </div>
-    </div>
+    <>
+      {items.map((item) => (
+        <form key={item.id}>
+          <label>
+            Время прибытия:
+            <input
+              type="text"
+              placeholder="Время прибытия"
+              value={arrTime}
+              onChange={(event: React.FormEvent<HTMLInputElement>) => {
+                setArrTime(event.currentTarget.value);
+                item.ArriveTime = event.currentTarget.value;
+              }}
+            />
+            <button
+              type="button"
+              onClick={() => {
+                item.ArriveTime = GetTime();
+                setArrTime(item.ArriveTime);
+              }}
+            >
+              Авто
+            </button>
+          </label>
+          <label>
+            Время начала обслуживания:
+            <input
+              type="text"
+              placeholder="Начало обслуживания"
+              onChange={(event: React.FormEvent<HTMLInputElement>) => {
+                item.StartTime = event.currentTarget.value;
+              }}
+            />
+            <button type="button">Авто</button>
+          </label>
+          <label>
+            Конец обслуживания:
+            <input
+              type="text"
+              placeholder="Конец обслуживания"
+              onChange={(event: React.FormEvent<HTMLInputElement>) => {
+                item.EndTime = event.currentTarget.value;
+              }}
+            />
+            <button type="button">Авто</button>
+          </label>
+        </form>
+      ))}
+      <button onClick={LogAllData}>Вывести данные</button>
+      <button
+        onClick={() => {
+          setItems([...items, new ItemProp(items.length + 1, "", "", "")]);
+        }}
+      >
+        Добавить строку
+      </button>
+    </>
   );
-}
+};
+
+export default FormComp;
